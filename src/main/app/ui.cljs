2645a6414666cc5173dd2d8c166bb11d21a74a10
Patrick K. Welter <pk2global@gmail.com>
2020-05-10 13:39:48 +0200

    Setup simple Eventhandler

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -1,14 +1,15 @@
 (ns app.ui
   (:require
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-   [com.fulcrologic.fulcro.dom :as dom :refer [h5 h4 div p li ul]]))
+   [com.fulcrologic.fulcro.dom :as dom :refer [h5 h4 div p li ul button]]
+   [app.mutations :as api]))
 
 
-(defsc Person [this {:person/keys [name age]}]
+(defsc Person [this {:person/keys [person/name person/age]} {:keys [onDelete]}]
   {:query [:person/name :person/age]
    :initial-state (fn [{:keys [name age] :as params}] {:person/name name :person/age age})}
   (li
-   (h5 (str name " (age " age ")"))))
+   (h5 (str name " (age " age ")") (button {:onClick #(onDelete name)} "X"))))
 
 (def ui-person (comp/factory Person {:keyfn :person/name}))
 
@@ -19,14 +20,15 @@
    (fn [{:keys [label]}]
      {:list/label  label
       :list/people (if (= label "Friends")
-                     [(comp/get-initial-state Person {:name "Sally" :age 32 :dosenbier "licherzzz"})
+                     [(comp/get-initial-state Person {:name "Sally" :age 32})
                       (comp/get-initial-state Person {:name "Joe" :age 22})]
                      [(comp/get-initial-state Person {:name "Fred" :age 11})
                       (comp/get-initial-state Person {:name "Bobby" :age 55})])})}
+  (let [delete-person (fn [name] (println label "asked to delete" name))]
   (div
    (h4 label)
    (ul
-    (map ui-person people))))
+    (map (fn [p] (ui-person (comp/computed p {:onDelete delete-person}))) people)))))
 
 (def ui-person-list (comp/factory PersonList))
 
